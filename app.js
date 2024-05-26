f2fb56281788da705d939f6e088810073a985d1c
William <wmayhood@outlook.com>
2024-05-26 19:58:14 -0400

    Books can now be added to library array and displayed in html container
    Organizing books in an array for display is a crucial aspect of this project to allow for easy removal and opening of books

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 
 ///
-function Book(name, author, pages, pubYear){
 
-    this.name = name;
-    this.author = author;
-    this.pages=pages;
-    this.pubYear = pubYear;
+const library = [];
+let container = document.querySelector(".container");
+function Book(Title, Author, Pages, PubYear){//constructor
 
-    this.printTitle = function() {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages=Pages;
+    this.PubYear = PubYear;
 
-        return (this.name +', ' + this.author + ', ' + this.pages +', ' + this.pubYear);
+
+}
+
+function addBook(book){
+
+    library.push(book);
+    const bookDiv = document.createElement('div');
+    bookDiv.className = "bookDiv";
+    for(const key in book){
+        if(book.hasOwnProperty(key)){//if book has this property
+            const bookField = book[key];//grab value
+            const bookContent = document.createElement('p');
+            bookContent.className = key;
+            bookContent.textContent = `${key}: ${bookField} `
+            bookDiv.appendChild(bookContent);
+            
+
+        }
+
+
     }
+    container.appendChild(bookDiv);
+    
+
+}
+
+function printLib(){
+console.log("The library contains: ")
+console.log(library);
 
 }
 
 let book1 = new Book("Lord of the Rings", "J.R. Tolken", 540, 1955);
+addBook(book1);
+printLib();
 
-console.log("book info: " + book1.printTitle());
+//console.log("book info: " + book1.printTitle());
